72a7a74505303908c0f3ce1fca1bac2db1c0fa37
mohannad balam <b.mohanned85@gmail.com>
2023-12-27 16:49:36 +0200

    retrieving questions and add pagination

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -13,7 +13,9 @@ class QuestionController extends Controller
      */
     public function index()
     {
-        //
+        $questions = Question::latest()->paginate(5);
+
+        return view('questions.index', compact('questions'));
     }
 
     /**
